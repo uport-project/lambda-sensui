a3ac5c4e5db6a5e5ec1d0259f79c31be3ebc6fa5
Cristóbal Castillo <cristobal.castillo@consensys.net>
2017-12-19 20:30:59 -0300

    implement db persistance

--- src/lib/ethereumMgr.js
+++ src/lib/ethereumMgr.js
@@ -3,7 +3,7 @@ import { TxRelay } from 'uport-identity'
 import Web3 from 'web3'
 import Contract from 'truffle-contract'
 import Promise from 'bluebird'
-import { Client } from 'pg'
+import { Client, Pool } from 'pg'
 
 const txRelayArtifact = TxRelay.v2
 
@@ -16,7 +16,8 @@ class EthereumMgr {
 
     this.web3s = {}
     this.txRelays = {}
-    this.client = new Client({
+
+    this.pool = new Pool({
       connectionString: this.pgUrl,
     })
     for (const network in networks) {
@@ -31,65 +32,49 @@ class EthereumMgr {
     return await this.web3s[networkName].eth.getBalanceAsync(address)
   }
 
+  async getDatabaseNonce(address, networkName) {
+    if (!address) throw ('no address')
+    if (!networkName) throw ('no networkName')
 
-  async getDatabaseNonce(address, networkName){
-    if(!address) throw ('no address')
-    if(!networkName) throw ('no networkName')
-    try {
-      await this.client.connect()
-      const res = await this.client.query(
-        "SELECT nonce \
-          FROM nonces \
-          WHERE address='$1' \
-          and network_name='$2'",
-        [address, networkName])
-      return res.rows[0];
-    } catch(e){
-      throw(e);
-    } finally {
-      await this.client.end()
-    }
+    const { rows } = await this.pool.query('SELECT nonce FROM nonces WHERE address = $1 and network=$2', [address, networkName])
+    console.log('current nonce:', rows[0])
+    return rows[0];
   }
 
-  async updateDatabaseNonce(address, networkName, nonce){
+  async insertDatabaseNonce(address, networkName, nonce, mode='insert'){
     if (!address) throw ('no address')
     if (!networkName) throw ('no networkName')
     if (!nonce) throw ('no nonce')
-    try {
-      await this.client.connect()
-      if(!this.getDatabaseNonce(address, networkName)){
-        //insert row since it doesn't exist on db
-        const res = await this.client.query(
-          "INSERT INTO nonces(address,nonce,network_name) \
-          VALUES('$1',$2,'$3') RETURNING nonces",
-          [address, 0, networkName])
-      } else {
-        const res = await this.client.query(
-          "UPDATE nonces \
-          SET nonce=$2 \
-          WHERE address='$1' \
-          AND network_name='$3' \
-          RETURNING nonces",
-          [address, nonce, networkName])
-      }
-    } catch (e) {
-      throw (e);
-    } finally {
-      await this.client.end()
-    }
 
+    const text = 'INSERT INTO nonces(address, nonce, network) \
+            VALUES($1, $2, $3) \
+            ON CONFLICT(address) DO UPDATE \
+            SET nonce=$2 \
+            RETURNING *'
+
+    const values = [address, nonce, networkName]
+    console.log("values", values)
+
+    try {
+      const res = await this.pool.query(text, values)
+      console.log(res.rows[0])
+    } catch (err) {
+      console.log(err.stack)
+    }
   }
 
   async getNonce(address, networkName) {
-    let dbNonce = await this.getDatabaseNonce(address, networkName)
-    let networkNonce = await this.web3s[networkName].eth.getTransactionCountAsync(address)
+    const dbNonce = await this.getDatabaseNonce(address, networkName)
+    const networkNonce = await this.web3s[networkName].eth.getTransactionCountAsync(address)
+    let nonce = 0
     if (dbNonce > networkNonce) {
-      this.updateDatabaseNonce(address, networkName, dbNonce+1)
+      nonce = dbNonce
     } else {
-      this.updateDatabaseNonce(address, networkName, networkNonce + 1)
+      nonce = networkNonce
     }
-    console.log(address, 'nonce:', networkNonce)
-    return networkNonce
+    await this.insertDatabaseNonce(address, networkName, nonce + 1)
+    console.log(address, 'nonce:', nonce)
+    return nonce
   }
 
   async sendRawTransaction(signedRawTx, networkName) {
