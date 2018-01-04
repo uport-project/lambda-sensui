3a5795824def55456e29d24b92747576adb19909
Andres Junge <ajunge@yaykuy.cl>
2018-01-04 00:27:06 -0300

    last fix

--- src/api-v2_handler.js
+++ src/api-v2_handler.js
@@ -27,33 +27,33 @@ const preHandler = (handler,event,context,callback) =>{
   }
 }
 
-const doHandler = (handler, event, context, callback) => {
-  handler.handle(event, context, (err, resp) => {
+const doHandler = (handler,event,context,callback) =>{
+  handler.handle(event,context,(err,resp)=>{
     let response;
-    if (err == null) {
+    if(err==null){
       response = {
-        statusCode: 200,
-        body:JSON.stringify({
-          status: 'success',
-          data: resp
-        })
-      }
-    } else {
-      console.log("ERROR: ", err.message);
-      let code = 500;
-      if (err.code) code = err.code;
-      let message = err;
-      if (err.message) message = err.message;
-
-      let body =
+          statusCode: 200,
+          body: JSON.stringify({
+            status: 'success',
+            data: resp
+          })
+        }
+    }else{
+      //console.log(err);
+      let code=500;
+      if(err.code) code=err.code;
+      let message=err;
+      if(err.message) message=err.message;
+      
       response = {
         statusCode: code,
         body: JSON.stringify({
           status: 'error',
-          message
+          message: message
         })
       }
     }
+
     callback(null, response)
   })
 
