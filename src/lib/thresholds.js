f69b4e28fc63a192e502dc18861cb6077d3195bd
Andres Junge <ajunge@yaykuy.cl>
2018-02-01 15:37:24 -0300

    checkBalances

---
+++ src/lib/thresholds.js
@@ -0,0 +1,19 @@
+module.exports = {
+    mainnet: {
+        master: 0,
+        develop: 0
+    },
+    rinkeby: {
+        master: 10000000000000000000,
+        develop: 5000000000000000000
+    },
+    kovan: {
+        master: 0,
+        develop: 0
+    },
+    ropsten: {
+        master: 0,
+        develop: 0
+    }
+  }
+  
