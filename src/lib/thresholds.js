87af9124b007284b771e98442f0424436581463f
Andres Junge <ajunge@yaykuy.cl>
2018-02-01 19:15:36 -0300

    kovan threshold

--- src/lib/thresholds.js
+++ src/lib/thresholds.js
@@ -8,8 +8,8 @@ module.exports = {
         develop:10000000000000000000
     },
     kovan: {
-        master: 0,
-        develop: 0
+        master: 15000000000000000000,
+        develop: 5000000000000000000
     },
     ropsten: {
         master: 0,
