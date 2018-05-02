9288b64a92812766eb576c0c329f18ead3082dce
Andres Junge <ajunge@yaykuy.cl>
2018-05-02 14:54:14 -0300

    fix label

--- sql/q_summary.sql
+++ sql/q_summary.sql
@@ -1,12 +1,12 @@
-SELECT day, network, sum(pending) as pending, sum(created) as created
+SELECT day, network, sum(pending) as pending, sum(mined) as mined
   FROM (
-	SELECT date_trunc('day', created) as day, network, count(*) as pending, 0 as created
+	SELECT date_trunc('day', created) as day, network, count(*) as pending, 0 as mined
 	  FROM tx
 	 WHERE tx_receipt IS NULL
 	   AND created < now() - INTERVAL '1 hour' --Only pending for more than 1 hour
 	 GROUP BY day,network
 	UNION ALL
-	SELECT date_trunc('day', created) as day, network, 0 as pending, count(*) as created
+	SELECT date_trunc('day', created) as day, network, 0 as pending, count(*) as mined
 	  FROM tx
 	 WHERE tx_receipt IS NOT NULL
 	 GROUP BY day,network
