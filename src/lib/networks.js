31310f3f3fadaacb80b4d74d67d8bf556b7375da
Andres Junge <ajunge@yaykuy.cl>
2019-03-18 15:18:23 -0300

    RSK networks

--- src/lib/networks.js
+++ src/lib/networks.js
@@ -8,10 +8,16 @@ module.exports = {
   '4': {
     rpcUrl: "https://rinkeby.infura.io/v3/47b5ecd1785e490f978d329febd4e1d5"
   },
+  '5': {
+    rpcUrl: "https://goerli.infura.io/v3/47b5ecd1785e490f978d329febd4e1d5"
+  },
   '42': {
     rpcUrl: "https://kovan.infura.io/v3/47b5ecd1785e490f978d329febd4e1d5"
   },
-  '5777': {
-    rpcUrl: "http://localhost:8545"
-  },
+  '30':{ //RSK Mainnet
+    rpcUrl: "http://35.183.134.62:4444/"
+  }, 
+  '31':{ //RSK Testnet
+    rpcUrl: "http://35.182.141.145:4444/"
+  }
 };
